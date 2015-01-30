1543e2e78f0620abdca50cbde128960d7ea15ac5
Zeke Nierenberg <hampzan09@gmail.com>
2015-01-30 10:26:38 -0500

    connecting to mongo

---
+++ models/index.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/wikistack');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
