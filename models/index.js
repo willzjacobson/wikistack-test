fbdc6b2b758deaf5d0022f122bb27529a8007247
Omri Bernstein <hippiccolo@gmail.com>
2015-09-30 02:00:59 -0400

    Minor tweaks

--- models/index.js
+++ models/index.js
@@ -27,17 +27,13 @@ pageSchema.methods.computeUrlName = function() {
 }
 
 pageSchema.statics.findByTag = function(tag, cb) {
-	this.find({ tags: { $elemMatch: { $eq: tag} } }, cb)
+	this.find({ tags: tag }, cb)
 }
 
 pageSchema.methods.getSimilar = function(cb) {
 	this.constructor.find({
 		_id: { $ne: this._id },
-		tags: {
-			$elemMatch: { 
-				$in: this.tags
-			}
-		}
+		tags: $in: this.tags
 	}, cb)
 }
 
